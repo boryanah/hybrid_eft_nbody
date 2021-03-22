2476e41f3bd37e5975552516c824c42c89efba2d
Boryana <boryana.hadzhiyska@cfa.harvard.edu>
2021-03-22 21:28:03 +0000

    latest plotting and loading data simple scripts

--- check_fit.py
+++ check_fit.py
@@ -1,28 +1,58 @@
+'''
+Usage:
+------
+
+python3 check_fit.py bf_bs_cosmo_dz_nuis_0_5 bf_b0_cosmo_dz_nuis_lowk_0_5 0
+python3 check_fit.py bf_bs_cosmo_dz_nuis_0_5 bf_b0_cosmo_dz_nuis_lowk_0_5 1
+
+
+'''
+import os
+import sys
+
 from getdist import plots, MCSamples
 import matplotlib
 #matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import getdist
 import numpy as np
-import os
 import sacc
 
-sG = sacc.Sacc.load_fits('data/clfid_covG.fits')
+# matplotlib settings
+from matplotlib import rc
+#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('font',**{'family':'serif','serif':['Times'],'size':14})
+rc('text', usetex=True)
 
-'''
-gcgc = getdist.loadMCSamples('./asDavid/cl_cross_corr_v2_gcgc-b_prior/2020-12-09_1000000_',
-                           settings={'ignore_rows':0.5})
-gcgc_np = getdist.loadMCSamples('./asDavid/cl_cross_corr_v2_gcgc_no-b-prior/2020-12-07_1000000_',
-                           settings={'ignore_rows':0.5})
+# color settings
+color_d = 'dodgerblue'
+color_b = '#CC6677'
+lss = ['-', '--']
+
+# load data files
+sG = sacc.Sacc.load_fits('data/cls_covG_covNG_DESgc_DESwl.fits')
+#nG = sacc.Sacc.load_fits('data/cls_nls_covG_covNG_DESgc_DESwl.fits')
+nG = sacc.Sacc.load_fits('/mnt/extraspace/gravityls_3/S8z/Cls_2/4096_asDavid_recompute_newfid/nls_covNG.fits')
+
+# name of the chain(s) we want to show
+chain_names = sys.argv[1:-1]
+#chain_name = chain_name[3:]
+want_ratio = bool(int(sys.argv[-1]))
+print("chain names = ", chain_names, len(chain_names))
 
 
-gcgc_di = np.load('./asDavid/cl_cross_corr_v2_gcgc-b_prior/cl_cross_corr_data_info.npz')
-gcgc_bfi = np.load('./asDavid/cl_cross_corr_v2_gcgc-b_prior/cl_cross_corr_bestfit_info.npz')
-'''
-chain_dir = '/home/boryanah/repos/montepython_public/chains/test/'
-gcgc_gcwl_wlwl_di = np.load(chain_dir+'cl_cross_corr_data_info.npz')
-gcgc_gcwl_wlwl_bfi = np.load(chain_dir+'cl_cross_corr_bestfit_info.npz')
-want_ratio = True
+# locating chains and files
+dis = []
+bfs = []
+for i in range(len(chain_names)):
+    chain_dir = os.path.expanduser('~/repos/montepython_public/chains/'+chain_names[i]+'/')
+    gcgc_gcwl_wlwl_di = np.load(chain_dir+'cl_cross_corr_data_info.npz')
+    gcgc_gcwl_wlwl_bfi = np.load(chain_dir+'cl_cross_corr_bestfit_info.npz')
+    dis.append(gcgc_gcwl_wlwl_di)
+    bfs.append(gcgc_gcwl_wlwl_bfi)
+
+if want_ratio: rat_lab = "chi2_"
+else: rat_lab = ""
 
 def split_vector_array(ells, chi2):
     ells_ar = []
@@ -46,18 +76,22 @@ def split_vector_array(ells, chi2):
     return ells_ar, chi2_ar
 
 
+def adjust_lightness(color, amount=0.5):
+    import matplotlib.colors as mc
+    import colorsys
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
+
 # GCGC
-info = gcgc_gcwl_wlwl_di
-bf = gcgc_gcwl_wlwl_bfi
-
-ells_bf_ar, cls_bf_ar = split_vector_array(info['ells'], bf['cls'])
-print(len(ells_bf_ar))
-
-f, axs = plt.subplots(1, 5, figsize=(15, 2.5), gridspec_kw={'wspace': 0}, sharey=True)
+f, axs = plt.subplots(1, 5, figsize=(18, 5.), gridspec_kw={'wspace': 0}, sharey=True)
 ax = axs.reshape((-1))
 
 ix = 0
-for i, trs in enumerate(info['tracers']):
+for i, trs in enumerate(dis[0]['tracers']):
     tr1, tr2 = trs
     
     if ('gc' not in tr1) or ('gc' not in tr2):
@@ -65,60 +99,162 @@ for i, trs in enumerate(info['tracers']):
     print(tr1, tr2)
     if ix == len(ax): break
     ell, cl, cov = sG.get_ell_cl('cl_00', tr1, tr2, return_cov=True)
+    ell, nl = nG.get_ell_cl('cl_00', tr1, tr2, return_cov=False)
     err = np.sqrt(np.diag(cov))
-    
-    
-    arg = np.argmin(np.abs(ells_bf_ar[i][0]-ell))
-    print("Fractional difference = ",(cl[arg:arg+len(ells_bf_ar[i])] - cls_bf_ar[i]) / err[arg:arg+len(ells_bf_ar[i])])
-    print(arg)
+        
     if want_ratio:
-        ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], cl[arg:arg+len(ells_bf_ar[i])]/cls_bf_ar[i], color='black', ls='--')
-    else:
-        ax[ix].errorbar(ell, cl, yerr=err, color='black', ls='--')
-        ax[ix].loglog(ells_bf_ar[i], cls_bf_ar[i], color='orange', lw=2)
-    ax[ix].text(0.9, 0.9, tr1+'-'+tr2, horizontalalignment='right', 
-               verticalalignment='top', transform=ax[ix].transAxes)
-    ix += 1
-    print(ix)
+        for j in range(len(chain_names)):
+            info = dis[j]
+            bf = bfs[j]
+            ells_bf_ar, cls_bf_ar = split_vector_array(info['ells'], bf['cls'])
+            
+            arg = np.argmin(np.abs(ells_bf_ar[i][0]-ell))
+            #rat = cl[arg:arg+len(ells_bf_ar[i])]/cls_bf_ar[i]
+            rat = (cl[arg:arg+len(ells_bf_ar[i])] - cls_bf_ar[i]) / err[arg:arg+len(ells_bf_ar[i])]
 
-plt.savefig("gcgc_fit.png")
+
+            if j == len(chain_names)-1:
+                label = 'Bestfit, DY1'
+            else:
+                label = 'Bestfit, HEFT'
+
+            ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], rat, color=adjust_lightness(color_b, 0.3+j*2./5.), ls=lss[j], lw=2+j/2., label=label)
+
+            if j == 0:
+                ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], np.zeros(len(ells_bf_ar[i])), color='silver')
+                ax[ix].fill_between(ell[arg:arg+len(ells_bf_ar[i])], np.zeros(len(ells_bf_ar[i]))-1., np.zeros(len(ells_bf_ar[i]))+1., color='gray', alpha=0.1)
+        
+    else:
+        ax[ix].errorbar(ell, cl, yerr=err, color=color_d, ls='', capsize=4, fmt='o', ms=4, label='Data')
+        #ax[ix].errorbar(ell, nl, yerr=np.zeros_like(err), color='gray', ls='', capsize=2, fmt='o', ms=2, label='Data + SN')
+        ax[ix].plot(ell, nl, color='gray', ls='--', label='Shotnoise')
+        for j in range(len(chain_names)):
+            info = dis[j]
+            bf = bfs[j]
+            ells_bf_ar, cls_bf_ar = split_vector_array(info['ells'], bf['cls'])
+            if j == len(chain_names)-1:
+                label = 'Bestfit, DY1'
+            else:
+                label = 'Bestfit, HEFT'
+            ax[ix].loglog(ells_bf_ar[i], cls_bf_ar[i], color=adjust_lightness(color_b, 0.3+j*2./5.), ls=lss[j], lw=2+j/2., label=label)
+            
+    if 'DESgc' in tr1:
+        label_tr1 = 'g'+tr1.split('DESgc')[-1]
+    else:
+        label_tr1 = 's'+tr1.split('DESwl')[-1]
+    if 'DESgc' in tr2:
+        label_tr2 = 'g'+tr2.split('DESgc')[-1]
+    else:
+        label_tr2 = 's'+tr2.split('DESwl')[-1]
+    ax[ix].text(0.9, 0.9, label_tr1+'-'+label_tr2, horizontalalignment='right', 
+               verticalalignment='top', transform=ax[ix].transAxes)
+
+    ax[ix].set_xlabel(r'$\ell$', fontsize=18)
+    if ix in [0]:
+        if want_ratio:
+            ax[ix].set_ylabel(r'$\frac{(C_{\ell, {\rm data}}-C_{\ell, {\rm bestfit}})}{\sigma_\ell}$', fontsize=18)
+        else:
+            ax[ix].set_ylabel(r'$C_\ell$', fontsize=18)
+
+    # TESTING
+    if want_ratio:
+        ax[ix].set_xlim([0.95*ells_bf_ar[i][0], 1.05*ells_bf_ar[i][-1]])
+    else:
+        ax[ix].set_xlim([0., 2000.])
+    ax[ix].axvline(x=0.3*ells_bf_ar[i][-1], ls=':', c='#CC6677')
+    
+    if ix == 0:
+        if want_ratio:
+            ax[i].legend(loc='upper left', frameon=False)
+        else:
+            ax[i].legend(loc='lower left', frameon=False)
+
+    print(ix)
+    ix += 1
+
+if len(chain_names) > 1:
+    plt.savefig("figs/"+rat_lab+"gcgc.pdf")
+else:
+    plt.savefig("figs/"+rat_lab+"gcgc_"+chain_name+".pdf")
 plt.close()
 
 # GCWL
-info = gcgc_gcwl_wlwl_di
-bf = gcgc_gcwl_wlwl_bfi
-
-ells_bf_ar, cls_bf_ar = split_vector_array(info['ells'], bf['cls'])
-f, axs = plt.subplots(5, 4, figsize=(15, 12))
+f, axs = plt.subplots(5, 4, figsize=(15, 15))
 ax = axs.reshape((-1))
 ix = 0
-for i, trs in enumerate(info['tracers']):
+for i, trs in enumerate(dis[0]['tracers']):
     tr1, tr2 = trs
-
     if ('gc' not in tr1) or ('wl' not in tr2):
         continue    
-    
+    print(tr1, tr2)
     ell, cl, cov = sG.get_ell_cl('cl_0e', tr1, tr2, return_cov=True)
     err = np.sqrt(np.diag(cov))
     
-    arg = np.argmin(np.abs(ells_bf_ar[i][0]-ell))
-    print("Fractional difference = ",(cl[arg:arg+len(ells_bf_ar[i])] - cls_bf_ar[i]) / err[arg:arg+len(ells_bf_ar[i])])
-    print(arg)
     if want_ratio:
-        ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], cl[arg:arg+len(ells_bf_ar[i])]/cls_bf_ar[i], color='black', ls='--')
-    else:
-        ax[ix].errorbar(ell, cl, yerr=err, color='k', ls='--')
-        ax[ix].loglog(ells_bf_ar[i], cls_bf_ar[i], color='orange', lw=2)
-    ax[ix].text(0.9, 0.9, tr1+'-'+tr2, horizontalalignment='right', 
-                verticalalignment='top', transform=ax[ix].transAxes)
-    ix += 1
-    print(ix)
+        for j in range(len(chain_names)):
+            info = dis[j]
+            bf = bfs[j]
+            ells_bf_ar, cls_bf_ar = split_vector_array(info['ells'], bf['cls'])
+            
+            arg = np.argmin(np.abs(ells_bf_ar[i][0]-ell))
+            #rat = cl[arg:arg+len(ells_bf_ar[i])]/cls_bf_ar[i]
+            rat = (cl[arg:arg+len(ells_bf_ar[i])] - cls_bf_ar[i]) / err[arg:arg+len(ells_bf_ar[i])]
+            ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], rat, color=adjust_lightness(color_b, 0.3+j*2./5.), ls=lss[j], lw=2+j/2.)
 
-plt.savefig("gcwl_fit.png")
+            if j == 0:
+                ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], np.zeros(len(ells_bf_ar[i])), color='silver')
+                ax[ix].fill_between(ell[arg:arg+len(ells_bf_ar[i])], np.zeros(len(ells_bf_ar[i]))-1., np.zeros(len(ells_bf_ar[i]))+1., color='gray', alpha=0.1)
+
+    else:
+        ax[ix].errorbar(ell, cl, yerr=err, color=color_d, ls='', capsize=4, fmt='o', ms=4, label='Data')
+        for j in range(len(chain_names)):
+            info = dis[j]
+            bf = bfs[j]
+            ells_bf_ar, cls_bf_ar = split_vector_array(info['ells'], bf['cls'])
+            if j == len(chain_names)-1:
+                label = 'Bestfit'
+            else:
+                label = ''
+            ax[ix].loglog(ells_bf_ar[i], cls_bf_ar[i], color=adjust_lightness(color_b, 0.3+j*2./5.), ls=lss[j], lw=2+j/2., label=label)
+
+    if 'DESgc' in tr1:
+        label_tr1 = 'g'+tr1.split('DESgc')[-1]
+    else:
+        label_tr1 = 's'+tr1.split('DESwl')[-1]
+    if 'DESgc' in tr2:
+        label_tr2 = 'g'+tr2.split('DESgc')[-1]
+    else:
+        label_tr2 = 's'+tr2.split('DESwl')[-1]
+    ax[ix].text(0.9, 0.9, label_tr1+'-'+label_tr2, horizontalalignment='right', 
+                verticalalignment='top', transform=ax[ix].transAxes)
+
+    if ix > 15:
+        ax[ix].set_xlabel(r'$\ell$', fontsize=18)
+    if ix in [0, 4, 8, 12, 16]:
+        if want_ratio:
+            ax[ix].set_ylabel(r'$\frac{(C_{\ell, {\rm data}}-C_{\ell, {\rm bestfit}})}{\sigma_\ell}$', fontsize=18)
+        else:
+            ax[ix].set_ylabel(r'$C_\ell$', fontsize=18)
+
+    # TESTING
+    if want_ratio:
+        ax[ix].set_xlim([0.95*ells_bf_ar[i][0], 1.05*ells_bf_ar[i][-1]])
+    else:
+        ax[ix].set_xlim([0., 2000.])
+    
+    #if ix == 0 and want_ratio == False:
+    #    ax[ix].legend(loc='lower left', frameon=False)
+    ix += 1
+
+if len(chain_names) > 1:
+    plt.savefig("figs/"+rat_lab+"gcwl.pdf")
+else:
+    plt.savefig("figs/"+rat_lab+"gcwl_"+chain_name+".pdf")
 #plt.show()
 plt.close()
 
-
+print("Skipping wlwl")
+quit()
 # WLWL
 info = gcgc_gcwl_wlwl_di
 bf = gcgc_gcwl_wlwl_bfi
@@ -136,17 +272,44 @@ for i, trs in enumerate(info['tracers']):
     err = np.sqrt(np.diag(cov))
     print(tr1, tr2)
     arg = np.argmin(np.abs(ells_bf_ar[i][0]-ell))
-    print("Fractional difference = ",(cl[arg:arg+len(ells_bf_ar[i])] - cls_bf_ar[i]) / err[arg:arg+len(ells_bf_ar[i])])
+    print("Fractional difference = ", (cl[arg:arg+len(ells_bf_ar[i])] - cls_bf_ar[i]) / err[arg:arg+len(ells_bf_ar[i])])
     print(arg, ix)
     if want_ratio:
-        ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], cl[arg:arg+len(ells_bf_ar[i])]/cls_bf_ar[i], color='black', ls='--')
+        #rat = cl[arg:arg+len(ells_bf_ar[i])]/cls_bf_ar[i]
+        rat = (cl[arg:arg+len(ells_bf_ar[i])] - cls_bf_ar[i]) / err[arg:arg+len(ells_bf_ar[i])]
+        ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], np.zeros(len(ells_bf_ar[i])), color='silver')
+        ax[ix].fill_between(ell[arg:arg+len(ells_bf_ar[i])], np.zeros(len(ells_bf_ar[i]))-1., np.zeros(len(ells_bf_ar[i]))+1., color='gray', alpha=0.1)
+        ax[ix].plot(ell[arg:arg+len(ells_bf_ar[i])], rat, color=color_b, ls='-')
     else:
-        ax[ix].errorbar(ell, cl, yerr=err, color='k', ls='--')
-        ax[ix].loglog(ells_bf_ar[i], cls_bf_ar[i], color='orange', lw=2.5)
-    ax[ix].text(0.9, 0.9, tr1+'-'+tr2, horizontalalignment='right', 
+        ax[ix].errorbar(ell, cl, yerr=err, color=color_d, ls='', capsize=2, fmt='o', ms=2)
+        ax[ix].loglog(ells_bf_ar[i], cls_bf_ar[i], color=color_b, alpha=0.7+j/10., lw=3)
+
+    if 'DESgc' in tr1:
+        label_tr1 = 'g'+tr1.split('DESgc')[-1]
+    else:
+        label_tr1 = 's'+tr1.split('DESwl')[-1]
+    if 'DESgc' in tr2:
+        label_tr2 = 'g'+tr2.split('DESgc')[-1]
+    else:
+        label_tr2 = 's'+tr2.split('DESwl')[-1]
+    ax[ix].text(0.9, 0.9, label_tr1+'-'+label_tr2, horizontalalignment='right', 
                verticalalignment='top', transform=ax[ix].transAxes)
-    #ix += 1
-plt.savefig("wlwl_fit.png")
+
+    if ix > 16:
+        ax[ix].set_xlabel(r'$\ell$')
+    if ix in [0, 4, 8, 12, 16]:
+        if want_ratio:
+            ax[ix].set_ylabel(r'$\frac{(C_{\ell, {\rm data}}-C_{\ell, {\rm bestfit}})}{\sigma_\ell}$', fontsize=18)
+        else:
+            ax[ix].set_ylabel(r'$C_\ell$')
+
+    # TESTING
+    ax[ix].set_xlim([0.95*ells_bf_ar[i][0], 1.05*ells_bf_ar[i][-1]])
+    
+    if ix == 0 and want_ratio == False:
+        plt.legend()
+    ix += 1
+
+plt.savefig("figs/"+rat_lab+"wlwl_"+chain_name+".png")
 #plt.show()
 plt.close()
-
--- read_data.py
+++ read_data.py
@@ -1,7 +1,10 @@
 import sacc
 from astropy.io import fits
+import numpy as np
 
-filename = "data/cls_covG_covNG_DESgc_DESwl.fits"
+#filename = "data/cls_covG_covNG_DESgc_DESwl.fits"
+#filename = "data/fiducial_nonoise.fits"
+filename = "/mnt/extraspace/gravityls_3/S8z/Cls_2/4096_asDavid_recompute_newfid/cls_covG_covNG_DESgc_DESwl.fits"
 
 s = sacc.Sacc.load_fits(filename)
 
@@ -19,7 +22,7 @@ for i, tracer in enumerate(s.tracers):
 
 for tr1, tr2 in s.get_tracer_combinations():
     print(tr1, tr2)
-quit()
+
     
 for i in range(len(dt)):
     t = s.get_tracer_combinations(dt[i])
@@ -29,14 +32,26 @@ for i in range(len(dt)):
     print(s.get_ell_cl(dt[i],t[0][0],t[0][1]))
 
     print(s.get_mean(dt[i],t[0]))
-    quit()
+
 
 hdu = fits.open(filename)
-
 cov = sacc.covariance.BaseCovariance.from_hdu(hdu['covariance'])
 
+mean = s.mean
+ndim = len(mean)
 icov = cov.inverse
 
+covmat_nn = cov.get_block(np.arange(ndim))
 
+#filename = "data/clfid_covG.fits"
+filename = "data/fiducial_noise.fits"
+hdu = fits.open(filename)
+cov = sacc.covariance.BaseCovariance.from_hdu(hdu['covariance'])
+
+covmat_n = cov.get_block(np.arange(ndim))
+
+print(covmat_nn-covmat_n)
+print(covmat_n[:10])
+print(np.sum(np.abs((covmat_nn-covmat_n)))/ndim**2)
 
 #dt=['cl_00', 'cl_0e', 'cl_0b', 'cl_ee', 'cl_eb', 'cl_be', 'cl_bb']
